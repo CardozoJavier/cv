b7131640b86a8829f1e1333dde4f909cf3219ec5
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-05-25 03:05:40 -0300

    Update colors palette

--- src/styles/palette.js
+++ src/styles/palette.js
@@ -2,6 +2,49 @@ const palette = {
   red46: '#d04646',
   black08: 'rgba(0, 0, 0, .8)',
   link: '#5a5af1',
+  white: '#fff',
+  black: '#000',
+  action: '#5A47A1',
+  purple90: '#5e5e90',
+  dark: {
+    primary: '#ff8364',
+    secondary: '#ffb677',
+    tertiary: '#ffecc7',
+    background: '#171725',
+  },
+  light: {
+    primary: 'rgba(0, 0, 0, .8)',
+    background: '#fff',
+  },
+  gray: {
+    g0: '#FBFBFD',
+    g05: '#CDD3E1',
+    g07: '#C5C3C3',
+    g1: '#EFF0F5',
+    g2: '#EAE9E9',
+    g3: '#9A9EA9',
+    g4: '#65686F',
+  },
+  violet: {
+    v05: '#F1F3FE',
+    v1: '#5A47A1',
+  },
+  green: {
+    g0: '#CAF6ED',
+    g1: '#00B58E',
+  },
+  blue: {
+    b05: '#D8EDFB',
+    b2: '#4BB3F9',
+  },
+  yellow: {
+    y0: '#FDF3DA',
+    y2: '#E29E22',
+  },
+  red: {
+    r1: '#FFE0DE',
+    r3: '#FF0000',
+  },
 };
 
 export default palette;
